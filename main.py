b7130df9459f0881bc696ae2d059efed5fe99362
cadenweigel <cadenweigel4@gmail.com>
2025-05-26 00:54:59 -0700

    added word count save

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import time
+import os
 from scraper import get_links, scrape_article
-from parser import count_word_frequencies
+from parser import (
+    count_word_frequencies, save_word_counts, load_word_counts,
+    convert_hash_to_alphabetical_list, convert_hash_to_sorted_list)
 from hash import HashTable   
 
 def main():
 
-    start_time = time.time()
-    links = get_links()
-    data = []
+    if os.path.exists("word_counts.pkl"):
+        print("Loading cached word counts...")
+        word_counts = load_word_counts()
+    else:
+        print("Scraping articles and computing word frequencies...")
+        start_time = time.time()
+        links = get_links()
+        data = [scrape_article(link) for link in links]
 
-    for link in links:
-        page_data = scrape_article(link)
-        data.append(page_data)
+        total_duration = time.time() - start_time
+        with open("log.txt", "a", encoding="utf-8") as f:
+            f.write(f"\nTotal scraping time: {total_duration:.2f} seconds\n")
 
-    total_duration = time.time() - start_time
-    with open("log.txt", "a", encoding="utf-8") as f:
-        f.write(f"\nTotal scraping time: {total_duration:.2f} seconds\n")
+        word_counts = count_word_frequencies(data)
+        save_word_counts(word_counts)
 
-    word_counts = count_word_frequencies(data)
-    print(word_counts)
+    sorted_count = convert_hash_to_sorted_list(word_counts)
+    print(sorted_count)
+    print(len(sorted_count))
+
 
 if __name__ == "__main__":
     main()
--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 import re
+import pickle
 from hash import HashTable   
 
 def count_word_frequencies(data):
@@ -16,3 +17,29 @@ def count_word_frequencies(data):
                     word_counts.set(word, 1)
 
     return word_counts
+
+def convert_hash_to_sorted_list(hash):
+    items = []
+    for bucket in hash.table:
+        for key, value in bucket:
+            items.append((key, value))
+    return sorted(items, key=lambda x: x[1], reverse=True)
+
+def convert_hash_to_alphabetical_list(hash):
+    items = []
+    for bucket in hash.table:
+        for key, value in bucket:
+            items.append((key, value))
+    return sorted(items, key=lambda x: x[0])
+
+def save_word_counts(word_counts, filename='word_counts.pkl'):
+    with open(filename, 'wb') as f:
+        pickle.dump(word_counts.table, f)
+
+def load_word_counts(filename='word_counts.pkl'):
+    with open(filename, 'rb') as f:
+        table = pickle.load(f)
+    
+    wc = HashTable()
+    wc.table = table
+    return wc
